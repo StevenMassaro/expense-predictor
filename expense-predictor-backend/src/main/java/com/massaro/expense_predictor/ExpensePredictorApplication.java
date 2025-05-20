46a21bfdb03514ae686f2a19166044dc15a39e47
Steven Massaro <steven.massaro.web@gmail.com>
2025-05-20 14:39:09 -0500

    initial add of backend

---
+++ expense-predictor-backend/src/main/java/com/massaro/expense_predictor/ExpensePredictorApplication.java
@@ -0,0 +1,13 @@
+package com.massaro.expense_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensePredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensePredictorApplication.class, args);
+	}
+
+}
